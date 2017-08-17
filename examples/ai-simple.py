21e11afc9a150c7151e7e56aff136175bea7b7b9
Ingo Blechschmidt <iblech@web.de>
2017-08-18 00:57:44 +0200

    typo

---
+++ examples/ai-simple.py
@@ -0,0 +1,3 @@
+def decide(mySeekers, goals, otherPlayers, world):
+  mySeekers[0].target = goals[0].position
+  return mySeekers
--- examples/simple.py
+++
@@ -1,5 +0,0 @@
-from seekers_types import *
-
-def decide(mySeekers, goals, otherPlayers, world):
-  mySeekers[0].position = goals[0].position
-  return mySeekers
