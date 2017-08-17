b83e26d5c15a73e2309b3e8dd275bc2b6386e673
Dominik Xaver Hörl <hoe.dom@gmx.de>
2017-08-17 18:47:26 +0200

    Add foldr1.

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,9 @@ def foldr(f,a,xs):
     a = f(a,x)
   return a
 
+def foldr1(f,xs):
+  return foldr(f,xs[0],xs[1:])
+
 def flatten(nested):
   for xs in nested:
     for x in xs:
@@ -12,5 +15,4 @@ def flatten(nested):
 
 def bump(r):
   return math.exp(1 / (r**2 - 1)) if r < 1 else 0
-
 
