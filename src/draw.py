57faf5d1399c87e89ac6786f08ebcb052698e949
Dominik Xaver Hörl <hoe.dom@gmx.de>
2017-08-18 17:12:28 +0200

    Use torus distance to draw jets.

--- src/draw.py
+++ src/draw.py
@@ -29,7 +29,7 @@ def draw(players, camps, goals, animations, world, screen):
   # draw jet streams
   for p in players:
     for s in p.seekers:
-      a = (s.target - s.position).normalized()
+      a = world.torus_direction(s.position, s.target)
       if (not s.disabled() and a.norm()>0):
         draw_jet_stream(s.position, -a, world, screen)
   # draw seekers
