86864a675b9c402b0edc0bdd22723a115ed63eb4
Sven Prüfer <pruefer.sven@gmail.com>
2018-06-30 22:41:36 +0200

    Made source code files GPL 3 compliant and added licence

--- src/hash_color.py
+++ src/hash_color.py
@@ -1,3 +1,20 @@
+# Copyright 2018 Matthias Hutzler
+#
+# This file is part of Seekers.
+#
+# Seekers is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Seekers is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Seekers.  If not, see <http://www.gnu.org/licenses/>.
+
 import hashlib
 import random
 import math
