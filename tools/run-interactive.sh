fcd2d8d68458c06de5272808fe28499fd98a48d9
Bela Tollkien <bela.tollking@gmail.com>
2022-10-27 01:49:11 +0200

    remove redundant tools folder

--- tools/run-interactive.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Simple script to run the simulation and restart it in case it crashes.
-# Later on, it will copy the user's bots to a tournament server.
-
-while :; do
-    python3 ~/seekers/src/seekers.py
-    sleep 0.5
-done
